f080e3c240605dbc4da08024dc5a9a5c0eddfcb0
MartinsAle <alessandro.martinns@gmail.com>
2024-03-21 10:15:41 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -38,12 +38,20 @@ public class Reservation {
 		//recuperando a diferença entre as datas em milisengundos
 		long diff = checkOut.getTime() - checkIn.getTime(); 
 		//convertendo o valor da variavel diff que está em milisegundos para dias
-		return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS); 
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); 
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates!";
+		}else if(!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date!";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		
+		return null;
 	}
 	
 	@Override
