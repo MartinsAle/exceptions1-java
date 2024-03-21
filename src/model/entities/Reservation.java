6841ac0776193f19cca2fa4631036ebdae33f10c
MartinsAle <alessandro.martinns@gmail.com>
2024-03-21 09:54:20 -0300

    Very bad solution

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,61 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		//recuperando a diferença entre as datas em milisengundos
+		long diff = checkOut.getTime() - checkIn.getTime(); 
+		//convertendo o valor da variavel diff que está em milisegundos para dias
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS); 
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
+	}
+}
